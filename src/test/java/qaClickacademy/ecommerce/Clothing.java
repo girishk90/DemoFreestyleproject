6b2eec562209b576097ba2561671fea91ad47c52
Girish <girishk90@gmail.com>
2022-02-15 22:21:24 +0530

    First commit

---
+++ src/test/java/qaClickacademy/ecommerce/Clothing.java
@@ -0,0 +1,25 @@
+package qaClickacademy.ecommerce;
+
+import org.testng.annotations.Test;
+
+public class Clothing {
+
+	@Test
+	public void Ladieswear()
+	{
+		System.out.println("Ladies clothing section");
+	}
+	
+	@Test
+	public void GentsWear()
+	{
+		System.out.println("Gents clothing section");
+	}
+	
+	@Test
+	public void KidsWear()
+	{
+		System.out.println("Kids clothing section");
+	}
+	
+}
---
+++ src/test/java/qaClickacademy/ecommerce/Electronics.java
@@ -0,0 +1,20 @@
+package qaClickacademy.ecommerce;
+
+import org.testng.annotations.Test;
+
+public class Electronics 
+{
+
+    @Test
+    public void Mobiles()
+    {
+     System.out.println("Mobile section");
+    }
+    
+    
+   @Test
+   public void Camera()
+   {
+	   System.out.println("Camera section");
+   }
+}
